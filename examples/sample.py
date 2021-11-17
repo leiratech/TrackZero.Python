7b8a3dc8ae1be24c7ff1d6fbdf84369914371417
Adel Alzubeir <adelzu@outlook.com>
2021-11-17 12:34:46 +0300

    initial

---
+++ examples/sample.py
@@ -0,0 +1,22 @@
+from TrackZero import TrackZero, entity
+from datetime import datetime
+from uuid import uuid4
+
+trackZero_client = TrackZero("{your api key here}")
+
+analytics_space_id = "python_example-3"
+
+trackZero_client.create_analytics_space(analytics_space_id)
+
+order = entity("Orders", 712) \
+            .add_attribute("Time", datetime.utcnow()) \
+            .add_attribute("Total Amount", uuid4()) \
+            .add_entity_reference_attribute("Items", "Products", 1) \
+            .add_entity_reference_attribute("Items", "Products", 2) \
+            .add_entity_reference_attribute("Order By", "Customers", 23)
+
+trackZero_client.upsert_entity(order, analytics_space_id)
+
+session = trackZero_client.create_analytics_space_session(analytics_space_id, 3600)
+
+trackZero_client.delete_analytics_space(analytics_space_id)
