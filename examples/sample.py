f4c631ae922692599a700dee3aa48e13b7f5e759
Adel Alzubeir <adelzu@outlook.com>
2021-11-17 15:33:23 +0300

    readme, packaged

--- examples/sample.py
+++ examples/sample.py
@@ -1,14 +1,14 @@
-from TrackZero import TrackZero, entity
+from TrackZero import TrackZero, Entity
 from datetime import datetime
 from uuid import uuid4
 
 trackZero_client = TrackZero("{your api key here}")
 
-analytics_space_id = "python_example-3"
+analytics_space_id = "python_example-space"
 
 trackZero_client.create_analytics_space(analytics_space_id)
 
-order = entity("Orders", 712) \
+order = Entity("Orders", 712) \
             .add_attribute("Time", datetime.utcnow()) \
             .add_attribute("Total Amount", uuid4()) \
             .add_entity_reference_attribute("Items", "Products", 1) \
@@ -19,4 +19,6 @@ trackZero_client.upsert_entity(order, analytics_space_id)
 
 session = trackZero_client.create_analytics_space_session(analytics_space_id, 3600)
 
-trackZero_client.delete_analytics_space(analytics_space_id)
+print(session.url)
+
+# trackZero_client.delete_analytics_space(analytics_space_id)
--- src/TrackZero_package/analytics_space_session.py
+++
@@ -1,5 +0,0 @@
-class space_session:
-    def __init__(self, is_success: bool, url: str, session_key: str) -> None:
-        self.is_success = is_success
-        self.url = url
-        self.session_key = session_key
